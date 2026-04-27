1f7b98f20fc01776a691f6ee9a3082a7e3b55b9c
franciscacpereira <franciscapereira@student.dei.uc.pt>
2026-04-27 16:51:00 +0100

    Regroup files into old-setup as backup

--- glibc-unit-tests/inputs/__gconv_translit_find_example.c
+++
@@ -1,9 +0,0 @@
-nt
-internal_function
-__gconv_translit_find (struct trans_struct *trans)
-{
-  /* Transliteration module loading has been removed because it never
-     worked as intended and suffered from a security vulnerability.
-     Consequently, this function always fails.  */
-  return 1;
-}
